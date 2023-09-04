aeac5dc5f04bbefe7865b31b8d931c477f131ff7
Phonicmas <jkskovlund@gmail.com>
2023-09-05 00:56:40 +0200

    Patch 5/8

--- 1.4/Source/Genes40k/Verb_CastAbility_DaemonSummon.cs
+++ 1.4/Source/Genes40k/Verb_CastAbility_DaemonSummon.cs
@@ -1,35 +1,35 @@
 ﻿using RimWorld;
 using UnityEngine;
 using Verse;
+using Core40k;
 
 namespace Genes40k
 {
     public class Verb_CastAbility_DaemonSummon : Verb_CastAbility
     {
-
         protected override bool TryCastShot()
         {
-            if (!ability.def.HasModExtension<DefModExtension_DaemonSummon>())
+            if (!ability.def.HasModExtension<DefModExtension_DaemonSummon>() || !base.TryCastShot())
             {
                 return false;
             }
 
             Pawn pawn = Caster as Pawn;
-            int scaleSkill = pawn.skills.GetSkill(ability.def.GetModExtension<DefModExtension_DaemonSummon>().statScale).Level;
-            
+            int scaleSkill = pawn.skills.GetSkill(ability.def.GetModExtension<DefModExtension_DaemonSummon>().statScale).levelInt;
+
+            if (scaleSkill < 1)
+            {
+                scaleSkill = 1;
+            }
+
             int summonAmount = Mathf.FloorToInt(scaleSkill / 2);
             PawnKindDef toSummon = ability.def.GetModExtension<DefModExtension_DaemonSummon>().daemonToSummon;
 
-            if (base.TryCastShot())
+            for (int i = 0; i < summonAmount; i++)
             {
-                for (int i = 0; i < summonAmount; i++)
-                {
-                    GenSpawn.Spawn(PawnGenerator.GeneratePawn(toSummon, pawn.Faction), currentTarget.Cell, pawn.Map);
-                }
-                return true;
+                GenSpawn.Spawn(PawnGenerator.GeneratePawn(toSummon, pawn.Faction), currentTarget.Cell, pawn.Map);
             }
-
-            return false;
+            return true;
         }
     }
 }
